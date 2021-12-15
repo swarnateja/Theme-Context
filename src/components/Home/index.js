ce225023800de70bbe9de09ac86b11b0b338cfd5
swarnateja <achantaswarnateja@gmail.com>
2021-12-15 02:03:08 +0000

    Theme Context

---
+++ src/components/Home/index.js
@@ -0,0 +1,32 @@
+import ThemeContext from '../../context/ThemeContext'
+import './index.css'
+import Navbar from '../Navbar'
+
+const Home = () => (
+  <ThemeContext.Consumer>
+    {value => {
+      const {isDarkTheme} = value
+      const homeBgClassName = isDarkTheme ? 'home-bg-dark' : 'home-bg-light'
+      const homeImageURL = isDarkTheme
+        ? 'https://assets.ccbp.in/frontend/react-js/home-dark-img.png'
+        : 'https://assets.ccbp.in/frontend/react-js/home-light-img.png'
+      const homeTextClassName = isDarkTheme
+        ? 'home-text-light'
+        : 'home-text-dark'
+
+      return (
+        <div className={`home-app-container ${homeBgClassName}`}>
+          <Navbar />
+          <div className="home-responsive-container">
+            <div className="home-container">
+              <img className="home-image" src={homeImageURL} alt="home" />
+              <h1 className={`home-heading ${homeTextClassName}`}>Home</h1>
+            </div>
+          </div>
+        </div>
+      )
+    }}
+  </ThemeContext.Consumer>
+)
+
+export default Home
